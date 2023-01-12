527f2f743160a6b9905076b42da39c188c13c6b0
Irham Maulana <i.maulana.azn@gmail.com>
2023-01-12 14:47:10 +0700

    fetching data when tags button clicked then fill the latest blog with the data

--- components/atoms/SearchWithTagsButton.tsx
+++ components/atoms/SearchWithTagsButton.tsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import { Button } from '@mui/material';
 
 interface SearchWithTagsButtonProps{
-  tag:string
+  tag:string,
+  setSearchedTag:any
 }
 
 export default function SearchWithTagsButton(params:SearchWithTagsButtonProps) {
-  const { tag } = params;
+  const { tag,setSearchedTag } = params;
   const [activeClass, setActiveClass] = useState('');
   return (
     <Button
       className={`search-with-tags-btn ${activeClass}`}
-      onClick={() => { setActiveClass(activeClass === 'active' ? '' : 'active'); }}
+      onClick={() => { setActiveClass(activeClass === 'active' ? '' : 'active'); setSearchedTag(tag) }}
       variant="outlined"
       sx={{
         borderColor: '#C4C4C4', color: '#666666', textTransform: 'Capitalize', fontWeight: '400', fontSize: '.9rem', padding: '.1rem 1.2rem', margin: '0 .7rem .7rem 0',
--- components/organism/SearchWIthTags/SearchWithTags.tsx
+++ components/organism/SearchWIthTags/SearchWithTags.tsx
@@ -3,20 +3,21 @@ import { Box } from '@mui/material';
 import Title from '../../atoms/Title';
 import SearchWithTagsButton from '../../atoms/SearchWithTagsButton';
 
-export default function SearchWithTags() {
+export default function SearchWithTags(props:any) {
+  const {setSearchedTag} = props;
   return (
     <Box>
       <Title marginBottom={{ sm: '3.4rem', xs: '1.6rem' }} mainTitle="Search" secondTitle="With Tags" />
       <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-        <SearchWithTagsButton tag="Travel" />
-        <SearchWithTagsButton tag="Lifestyle" />
-        <SearchWithTagsButton tag="Fashion" />
-        <SearchWithTagsButton tag="Technology" />
-        <SearchWithTagsButton tag="Business" />
-        <SearchWithTagsButton tag="Desain" />
-        <SearchWithTagsButton tag="Health" />
-        <SearchWithTagsButton tag="Food" />
-        <SearchWithTagsButton tag="art" />
+        <SearchWithTagsButton tag="Travel" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Lifestyle" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Fashion" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Technology" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Business" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Desain" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Health" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="Food" setSearchedTag={setSearchedTag}/>
+        <SearchWithTagsButton tag="art" setSearchedTag={setSearchedTag}/>
       </Box>
     </Box>
   );
