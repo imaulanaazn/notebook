656611daa026735cecbed8a207a358b447f21041
Irham Maulana <i.maulana.azn@gmail.com>
2023-01-12 13:37:07 +0700

    removing author profile, changing date format, truncate des. add link to blog on featured card

--- components/organism/Featured/index.tsx
+++ components/organism/Featured/index.tsx
@@ -15,58 +15,58 @@ export default function Featured({ featuredData }:any) {
           }}
         >
           {featuredData?.articles?.slice(0, 13).map((blog:any, index:number) => (
-            <Box
-              key={`featured${index}`}
-              className="featured-card1"
-              sx={{
-                width: '100%', minWidth: '197px',
-              }}
-            >
-              <Typography
+            <a href={blog.url} target="_blank" key={`featured${index}`} rel="noreferrer" >
+              <Box
+                className="featured-card1"
                 sx={{
-                  fontWeight: '400', fontSize: { md: '0.74rem', sm: '0.8rem' }, display: 'inline-block', backgroundColor: '#E8F3F3', color: '#666666', padding: '.1rem .7rem', borderRadius: '3px', marginBottom: '0.344rem',
+                  width: '100%', minWidth: '197px',
                 }}
               >
-                {blog.source.name}
-              </Typography>
-              <Typography variant="h2" sx={{ fontSize: { md: '1.44rem', sm: '1.6rem', xs: '1.3rem' }, fontStyle: 'normal', fontWeight: '600' }}>{blog.title}</Typography>
-              <Box sx={{
-                width: '100%', height: {xs:'16rem', sm:'12rem',md:'13rem',lg:'14rem'}, backgroundColor: '#D9D9D9', borderRadius: '0.438rem', margin: '1.5rem 0 1.688rem 0', overflow: 'hidden',
-              }}
-              >
-                <img src={blog.urlToImage} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="" />
-              </Box>
-              <Stack direction="row">
-                <Stack direction="row" sx={{ alignItems: 'center' }}>
-                  <img src="img/author.png" alt="" style={{ width: '1rem', height: '1rem' }} />
-                  <Typography sx={{
-                    color: '#777777', fontWeight: '400', fontSize: '0.8rem', margin: '0 .5rem',
+                <Typography
+                  sx={{
+                    fontWeight: '400', fontSize: { md: '0.74rem', sm: '0.8rem' }, display: 'inline-block', backgroundColor: '#E8F3F3', color: '#666666', padding: '.1rem .7rem', borderRadius: '3px', marginBottom: '0.344rem',
                   }}
-                  >
-                    {blog.author}
-                  </Typography>
-                </Stack>
-                <Stack direction="row" sx={{ borderLeft: '1px solid #999999', borderRight: '1px solid #999999', padding: '0 .5rem' }}>
-                  <img src="icon/calendar.svg" alt="" style={{ width: '.8rem' }} />
-                  <Typography sx={{
-                    color: '#777777', fontWeight: '400', fontSize: '0.8rem', marginLeft: '.5rem',
-                  }}
-                  >
-                    {new Date(blog.publishedAt).toDateString()}
-                  </Typography>
-                </Stack>
+                >
+                  {blog.source.name}
+                </Typography>
+                <Typography variant="h2" sx={{ fontSize: { md: '1.44rem', sm: '1.6rem', xs: '1.3rem' }, fontStyle: 'normal', fontWeight: '600' }}>{blog.title}</Typography>
+                <Box sx={{
+                  width: '100%', height: {xs:'16rem', sm:'12rem',md:'13rem',lg:'14rem'}, backgroundColor: '#D9D9D9', borderRadius: '0.438rem', margin: '1.5rem 0 1.688rem 0', overflow: 'hidden',
+                }}
+                >
+                  <img src={blog.urlToImage} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="" />
+                </Box>
                 <Stack direction="row">
-                  <img src="icon/clock.svg" alt="" style={{ width: '.8rem', margin: '0 .5rem' }} />
-                  <Typography sx={{ color: '#777777', fontWeight: '400', fontSize: '0.8rem' }}>3 Min. To Read</Typography>
+                  <Stack direction="row" sx={{ alignItems: 'center' }}>
+                    <Typography sx={{
+                      color: '#777777', fontWeight: '400', fontSize: '0.8rem', margin: '0 .5rem',
+                    }}
+                    >
+                      {blog.author}
+                    </Typography>
+                  </Stack>
+                  <Stack direction="row" sx={{ borderLeft: '1px solid #999999', borderRight: '1px solid #999999', padding: '0 .5rem', alignItems: 'center' }}>
+                    <img src="icon/calendar.svg" alt="" style={{ width: '.8rem' }} />
+                    <Typography sx={{
+                      color: '#777777', fontWeight: '400', fontSize: '0.8rem', marginLeft: '.5rem',
+                    }}
+                    >
+                      {new Date(blog.publishedAt).toLocaleDateString()}
+                    </Typography>
+                  </Stack>
+                  <Stack direction="row">
+                    <img src="icon/clock.svg" alt="" style={{ width: '.8rem', margin: '0 .5rem' }} />
+                    <Typography sx={{ color: '#777777', fontWeight: '400', fontSize: '0.8rem' }}>{Math.round(blog.content.split(" ").length / 4)} Min. To Read</Typography>
+                  </Stack>
                 </Stack>
-              </Stack>
-              <Typography sx={{
-                color: '#555555', fontSize: {xs:'0.8rem',lg:'0.85rem'}, fontWeight: '400', marginTop: '1.188rem'
-              }}
-              >
-                {blog.description}
-              </Typography>
-            </Box>
+                <Typography sx={{
+                  color: '#555555', fontSize: {xs:'0.8rem',lg:'0.85rem'}, fontWeight: '400', marginTop: '1.188rem'
+                }}
+                >
+                  {blog.description.length > 100 ? blog.description.slice(0,150) + '...' : blog.description}
+                </Typography>
+              </Box>
+            </a>
           ))}
         </Box>
       </Box>
